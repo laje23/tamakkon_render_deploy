de24eed16dfe7cc315e7ce72ae21ffcd4867a67b
laje23 <a763545@gmail.com>
2025-09-21 00:17:27 +0330

    ...

--- send_message_handler.py
+++ send_message_handler.py
@@ -58,8 +58,8 @@ async def auto_send_not():
         if file_id:
             file = await file_id_to_bynery(file_id, bale_bot)
             if media_type == "photo":
-                await bale_bot.send_photo(bale_channel_id, file.read(), messages[0])
-                await eitaa_bot.send_file(eitaa_channel_id, file, messages[0])
+                await bale_bot.send_photo(bale_channel_id, file.read(), process_note_message(messages[0],id))
+                await eitaa_bot.send_file(eitaa_channel_id, file, process_note_message(messages[0],id))
                 messages.pop(0)
 
         for text in messages:
